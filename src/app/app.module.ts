9f24264858dd1aa6b851551c0b650f2bdfc15559
saigowthamr <saigowthamr@gmail.com>
2019-07-07 08:24:38 +0530

    programmatic navigation added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
@@ -31,6 +32,7 @@ const appRoutes: Routes = [
   ],
   imports: [
     BrowserModule,
+    FormsModule,
     RouterModule.forRoot(appRoutes)
   ],
   providers: [],
--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-contact',
@@ -6,10 +7,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./contact.component.css']
 })
 export class ContactComponent implements OnInit {
+  user = {
+    name: '',
+    email: ''
+  };
 
-  constructor() { }
+  // injecting route object into this contact component
+  constructor(private route: Router) { }
 
   ngOnInit() {
   }
+  onSubmit() {
+    console.log(this.user.name, this.user.email);
 
+    // inside array we need to pass the path we need to naviagte
+    this.route.navigate(['/users', '1']);
+  }
 }
